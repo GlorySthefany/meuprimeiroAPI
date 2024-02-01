953ddda29732118fb50e0c56a7b6abef64f9b364
GlorySthefany <156228699+GlorySthefany@users.noreply.github.com>
2024-02-01 17:33:41 -0300

    primeiro usuario criado

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import express from 'express';
-import {db, firestore} from '../bancodedados/firebase';
+import { db, firestore } from '../bancodedados/firebase';
 
 const app = express();
 
-app.listen(3000, function(){
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("Bem vindo ap APP!!!")
+})
+
+app.post('/usuario', async (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email
+    const telefone = req.body.telefone
+
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuarios'), {
+            nome: nome,
+            email: email,
+            telefone: telefone
+        })
+
+        return res.send('Usuario cadastrado com sucesso: ' + docRef.id)
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error)
+
+    }
+})
+
+
+app.listen(3000, function () {
     console.log("servidor rodando na porta http://localhost:3000!")
 });
